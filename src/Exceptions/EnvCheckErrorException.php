f26895f2176bb7478a71838ee59a9517b8548db7
Manabu Matsui <manabu.matsui@gmail.com>
2018-04-11 15:02:30 +0900

    forget to add

---
+++ src/Exceptions/EnvCheckErrorException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: matsui
+ * Date: 2018/04/11
+ * Time: 14:45
+ */
+
+namespace Crhg\EnvCheck\Exceptions;
+
+
+class EnvCheckErrorException extends \Exception
+{
+
+}
